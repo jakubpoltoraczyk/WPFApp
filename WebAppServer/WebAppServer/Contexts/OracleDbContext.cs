18916d4faadd03e6f2f3848f3da8d96860d4af6b
piotex <pkubon2@gmail.com>
2022-03-26 18:46:10 +0100

    test commit

---
+++ WebAppServer/WebAppServer/Contexts/OracleDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppServer.Models;
+
+namespace WebAppServer.Contexts
+{
+    public class OracleDbContext : DbContext
+    {
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options) { }
+
+        public DbSet<UserCategory> UserCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = @"C:\Users\pkubo\OneDrive\Dokumenty\GitHub\_Keys\oracle_db_ConnectionString.txt";
+
+            optionsBuilder.UseOracle(File.ReadAllText(path));
+        }
+    }
+}
---
+++ WebAppServer/WebAppServer/Models/UserCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppServer.Models
+{
+    public class UserCategory
+    {
+        public int UserCategoryId { get; set; }
+        public string UserCategoryName { get; set; }
+    }
+}
