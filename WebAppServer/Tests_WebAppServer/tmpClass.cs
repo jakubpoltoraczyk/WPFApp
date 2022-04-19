a658e1ce0902fb185f5ced76513e85bbd2eddcda
piotex <pkubon2@gmail.com>
2022-04-19 19:15:37 +0200

    tmp

---
+++ WebAppServer/Tests_WebAppServer/tmpClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using WebAppServer.Contexts;
+using WebAppServer.Controllers;
+using WebAppServer.Controllers.DbBasicControllers;
+using WebAppServer.Models;
+using WebAppServer.MoqModels;
+
+namespace Tests_WebAppServer
+{
+    class tmpClass
+    {
+        [Test]
+        public void ControlerTest()
+        {
+            var aaa = MoqCareScheduleList.GetInstance().GetMoqList();
+
+
+        }
+    }
+}
--- WebAppServer/WebAppServer/MoqModels/MoqCareScheduleList.cs
+++ WebAppServer/WebAppServer/MoqModels/MoqCareScheduleList.cs
@@ -17,13 +17,13 @@ namespace WebAppServer.MoqModels
                 _instance = new MoqCareScheduleList();
                 _data = new List<CareSchedule>()
                 {
-                    new CareSchedule(){ CareScheduleId=1, TimeOfCare=new DateTime(0,0,7,2,0,0), PaletPlantsType_Id=1, PriorityNumber=5, TypeOfCare_Id=3 },
-                    new CareSchedule(){ CareScheduleId=2, TimeOfCare=new DateTime(0,0,1,5,0,0), PaletPlantsType_Id=1, PriorityNumber=2, TypeOfCare_Id=4 },
-                    new CareSchedule(){ CareScheduleId=3, TimeOfCare=new DateTime(0,0,2,6,0,0), PaletPlantsType_Id=2, PriorityNumber=2, TypeOfCare_Id=6 },
-                    new CareSchedule(){ CareScheduleId=4, TimeOfCare=new DateTime(0,0,3,4,0,0), PaletPlantsType_Id=2, PriorityNumber=3, TypeOfCare_Id=2 },
-                    new CareSchedule(){ CareScheduleId=5, TimeOfCare=new DateTime(0,0,7,2,0,0), PaletPlantsType_Id=3, PriorityNumber=2, TypeOfCare_Id=1 },
-                    new CareSchedule(){ CareScheduleId=6, TimeOfCare=new DateTime(0,0,5,9,0,0), PaletPlantsType_Id=4, PriorityNumber=2, TypeOfCare_Id=3 },
-                    new CareSchedule(){ CareScheduleId=7, TimeOfCare=new DateTime(0,0,7,1,0,0), PaletPlantsType_Id=5, PriorityNumber=1, TypeOfCare_Id=5 },
+                    new CareSchedule(){ CareScheduleId=1, TimeOfCare=new TimeSpan(7,2,0,0), PaletPlantsType_Id=1, PriorityNumber=5, TypeOfCare_Id=3 },
+                    new CareSchedule(){ CareScheduleId=2, TimeOfCare=new TimeSpan(1,5,0,0), PaletPlantsType_Id=1, PriorityNumber=2, TypeOfCare_Id=4 },
+                    new CareSchedule(){ CareScheduleId=3, TimeOfCare=new TimeSpan(2,6,0,0), PaletPlantsType_Id=2, PriorityNumber=2, TypeOfCare_Id=6 },
+                    new CareSchedule(){ CareScheduleId=4, TimeOfCare=new TimeSpan(3,4,0,0), PaletPlantsType_Id=2, PriorityNumber=3, TypeOfCare_Id=2 },
+                    new CareSchedule(){ CareScheduleId=5, TimeOfCare=new TimeSpan(7,2,0,0), PaletPlantsType_Id=3, PriorityNumber=2, TypeOfCare_Id=1 },
+                    new CareSchedule(){ CareScheduleId=6, TimeOfCare=new TimeSpan(5,9,0,0), PaletPlantsType_Id=4, PriorityNumber=2, TypeOfCare_Id=3 },
+                    new CareSchedule(){ CareScheduleId=7, TimeOfCare=new TimeSpan(7,1,0,0), PaletPlantsType_Id=5, PriorityNumber=1, TypeOfCare_Id=5 },
                 };
             }
             return _instance;
